f550194555c88bb23bb9c519f4d7c15dbc927376
Subash Prakash <subashprakash8@gmail.com>
2023-10-09 22:57:22 +0200

    GENAG23-1: Update to the module

--- deploy/deploy/db_stack.py
+++ deploy/deploy/db_stack.py
@@ -14,13 +14,20 @@ class DBStack(Stack):
         self.instance = rds.DatabaseInstance(self, "Instance",
                                         engine=rds.DatabaseInstanceEngine.postgres(version=rds.PostgresEngineVersion.VER_15_4),
                                         instance_type=ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MEDIUM),
-                                        credentials=rds.Credentials.from_generated_secret("postgres", secret_name="RDS_postgres", exclude_characters="!&*^#@()"),
+                                        # Does not work by updating stack. Best create secret separate and pass it.
+                                        credentials=rds.Credentials.from_generated_secret("postgres", secret_name="RDS_postgres", exclude_characters="!&*^#@()}{[]\\><?%/"),
                                         vpc=vpc,
                                         vpc_subnets=ec2.SubnetSelection(
                                             subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS
                                         ),
+                                        ca_certificate=rds.CaCertificate.RDS_CA_RDS2048_G1,
                                         max_allocated_storage=200,
-                                        cloudwatch_logs_exports=["postgresql"]
+                                        cloudwatch_logs_exports=["postgresql"],
+                                        # Set true if prod
+                                        multi_az=False,
+
+
+
         )
 
     @property
