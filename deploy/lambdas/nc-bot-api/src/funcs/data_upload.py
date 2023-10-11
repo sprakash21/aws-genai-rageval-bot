314da26462839b3b0cdd782cd759f06af60c3fd7
Subash Prakash <subashprakash8@gmail.com>
2023-10-11 22:49:48 +0200

    Data upload to S3 from Streamlit

--- deploy/lambdas/nc-bot-api/src/funcs/data_upload.py
+++ deploy/lambdas/nc-bot-api/src/funcs/data_upload.py
@@ -1,3 +1,5 @@
+
+
 def lambda_handler(event, context):
     # Handling of the data upload of pdf into vectorstore
     pass
--- nc-bot/pages/01_data_uploader.py
+++ nc-bot/pages/01_data_uploader.py
@@ -1,5 +1,9 @@
+import os
 import streamlit as st
-from helpers.file_upload_helper import FileUpload
+from src.helpers.file_upload_helper import S3FileUpload
+from dotenv import load_dotenv
+
+load_dotenv()
 
 st.sidebar.markdown("# Data Uploader")
 st.title("Data Uploader")
@@ -8,5 +12,11 @@ st.caption("Extend the Vector Database by uploading Pdf data")
 uploaded_files = st.file_uploader("Choose a PDF File", accept_multiple_files=True, type=["pdf"])
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
+    s3_client = S3FileUpload()
+    fname = uploaded_file.name.replace(" ", "-")
+    status = s3_client.put_object(bytes_data, fname)
+    if status:
+        st.write(f"{uploaded_file.name} is uploaded successfully to S3")
+        bucket_name = os.environ.get("BUCKET_NAME")
+        s3_uri = f"https://{bucket_name}.s3.eu-central-1.amazonaws.com/{fname}"
+        st.write(f"You can access it from - {s3_uri}")
