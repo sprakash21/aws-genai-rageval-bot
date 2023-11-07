86ae5cab9f21b1c7f07db67e4d6fc72bfc0c86d0
Muhammad Taha Khan <muhammad.taha-khan@nordcloud.com>
2023-11-07 17:50:00 +0000

    Merged in fix/fix-data-uploader (pull request #9)
    fix data uploader.
    
    * fix data uploader.

--- nc-bot/pages/01_data_uploader.py
+++ nc-bot/pages/01_data_uploader.py
@@ -4,6 +4,7 @@ from src.config.app_config import get_db_type
 from src.helpers.file_upload_helper import S3FileUpload
 from src.helpers.upload_vectordb_helper import UploadHelper
 from dotenv import load_dotenv
+import boto3
 
 load_dotenv()
 
@@ -21,14 +22,17 @@ for uploaded_file in uploaded_files:
     status = s3_client.put_object(bytes_data, "pdf_data/" + fname)
     if status:
         st.write(f"{uploaded_file.name} is uploaded successfully to S3")
+        s3_boto3_client = boto3.client("s3")
         # Get the bucket_name from SSM parameters
         bucket_name = os.environ.get("BUCKET_NAME")
-        presigned_source_url = s3_client.generate_presigned_url(
+        presigned_source_url = s3_boto3_client.generate_presigned_url(
             "get_object",
             Params={"Bucket": bucket_name, "Key": f"pdf_data/{fname}"},
             ExpiresIn=600,
         )
-        st.write(f"You can access it from - {presigned_source_url}")
+        st.write(
+            f"You can access it from - <a href='{presigned_source_url}'>pdf_data/{fname}</a>"
+        )
         # Set to true only for using local database.
         db_local = get_db_type()
         upload_helper = UploadHelper(db_local=db_local)
